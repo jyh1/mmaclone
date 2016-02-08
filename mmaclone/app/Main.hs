2e52c7fdb987a38a24fd1f6cb1bf501aad7e6b7f
jyh1 <jyh1@mail.ustc.edu.cn>
2016-02-09 00:49:34 +0800

    compile succeed

--- mmaclone/app/Main.hs
+++ mmaclone/app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 import Control.Monad
 import Control.Monad.Except
 import Text.Printf
+import System.IO
 
 import Data.DataType
 import Eval.Eval
@@ -21,11 +22,11 @@ loop env n = do
 
 repl :: Env -> Int -> IOThrowsError LispVal
 repl env n = do
-  lift $ printf "In[%d]:=" n
+  lift $ printf "In[%d]:=" n >> hFlush stdout
   expr <- ExceptT (liftM readExpr getLine)
   evalWithRecord env n expr
 
 report :: Int -> ThrowsError LispVal -> IO ()
 report _ (Left err) = print err
 report _ (Right (Atom "Null")) = return ()
-report n (Right val) = printf "Out[%d]=" n >> putStrLn (show val)
+report n (Right val) = printf "Out[%d]=" n >> print val
